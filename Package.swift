a9d50a82845e73373e9efcb99364cfb0034a0136
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-11-26 00:14:22 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         //.package(name: "VarInt", path: "../VarInt") //Use this when attempting to run update-codecs (until our repo is public)
-        .package(url: "https://github.com/swift-libp2p/swift-varint.git", from: "0.0.1")
+        .package(url: "https://github.com/timechain-academy/swift-varint.git", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
