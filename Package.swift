2beb51dea4928e674426c1bf3300a09f66e14fc0
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-12-04 22:10:30 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -27,9 +27,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .executableTarget(
-            name: "Updater",
-            dependencies: []),
+        //.executableTarget(
+          //  name: "Updater",
+            //dependencies: []),
         .target(
             name: "Multicodec",
             dependencies: [
